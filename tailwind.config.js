76245129c0bbbf2ec43041c0704cba988c1e6e35
Simon Wasle <simon.wasle@gmail.com>
2025-08-06 10:29:26 +0200

    remove unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,6 @@ module.exports = {
     "./src/**/*.{clj,cljs,cljc}",
     "./resources/**/*.html"
   ],
-  safelist: [
-    // Button variant classes that are dynamically generated via (str "btn btn-" (name variant))
-    // Tailwind's purging can't detect these since they're not literal strings in the source
-    'btn-primary',
-    'btn-secondary', 
-    'btn-danger'
-  ],
   theme: {
     extend: {
       fontFamily: {
